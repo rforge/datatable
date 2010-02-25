134fb42058126f5df84cab89e323a2e8a5196482
mdowle <mdowle@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-25 23:36:48 +0000

    Now pre-allocating result table for 'by' and dogroups populates it. Avoids an intermediate list of results (which would have many small objects on the heap) so less memory required,  and avoids the need to unlist that list (faster). Usual silent vector recycling within groups is implemented in a fast way inside dogroups (so j can be list() always and no need for j=DT() to do recylcling which slowed things down).
    test.data.table not yet working as some more work to do: coerce single name j to list first, put colnames on ans, and get incbycols working.
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/datatable@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branch2/src/dogroups.c
+++ branch2/src/dogroups.c
@@ -10,17 +10,17 @@
 EXPORT SEXP dogroups();
 #endif
 
-SEXP dogroups(SEXP dt, SEXP SD, SEXP dtcols, SEXP order, SEXP starts, SEXP lens, SEXP jexp, SEXP env)
+SEXP dogroups(SEXP dt, SEXP SD, SEXP dtcols, SEXP order, SEXP starts, SEXP lens, SEXP jexp, SEXP env, SEXP testj, SEXP byretn)
 {
-  R_len_t i, j, k, n, rownum;
-  SEXP names, ans, sizes;
+  R_len_t i, j, k, n, rownum, ngrp, njval, ansloc, maxn, r, thisansloc, thislen;
+  SEXP names, ans, sizes, jval, jsizes;
 	if (TYPEOF(order) != INTSXP) error("order not integer");
   if (TYPEOF(starts) != INTSXP) error("starts not integer");
   if (TYPEOF(lens) != INTSXP) error("lens not integer");
   if (INTEGER(starts)[0]<1 || INTEGER(lens)[0]<1) error("starts[1]<1 or lens[1]<1");
   if(!isEnvironment(env)) error("’env’ should be an environment");
-  n = length(starts);  // the number of groups
-  PROTECT(ans = allocVector(VECSXP, n));
+  ngrp = length(starts);  // the number of groups
+  njval = length(testj);
 	names = getAttrib(SD, R_NamesSymbol);
 	// only the subset of column names that the j expression uses exist in SD, or if
 	// the j uses .SD (or .SDF), then all the columns of dt (detected in the calling R). 
@@ -43,12 +43,33 @@ SEXP dogroups(SEXP dt, SEXP SD, SEXP dtcols, SEXP order, SEXP starts, SEXP lens,
   	defineVar(install(CHAR(STRING_ELT(names, i))), VECTOR_ELT(SD, i), env);
   }
 	defineVar(install(".SD"), SD, env);
-	// By installing SD directly inside itself, R finds this symbol more quickly (if used). This then allows the
+	// By installing .SD directly inside itself, R finds this symbol more quickly (if used). This then allows the
 	// env's parent to be the one that called [.data.table, rather than the local scope of
 	// [.data.table, for robustness and efficiency. If it were local scope of [.data.table then user's j
 	// may inadvertently see interal variables names (as it used to in v1.2) and we had to name them to avoid
-	// conflict e.g. "o__".  That is no longer required.
-	for(i = 0; i < n; i++) {
+	// conflict e.g. "o__".  That is no longer required and things are much cleaner now.
+	
+  PROTECT(ans = allocVector(VECSXP, njval));
+  PROTECT(jsizes = allocVector(INTSXP, njval));
+  for(i = 0; i < njval; i++) {
+    SET_VECTOR_ELT(ans, i, allocVector(TYPEOF(VECTOR_ELT(testj, i)), INTEGER(byretn)[0]));
+    switch (TYPEOF(VECTOR_ELT(testj, i))) {  // TO DO : find a size table in a .h to use and save the switch (couldn't find one when looked)
+			case INTSXP :
+			case LGLSXP :
+				INTEGER(jsizes)[i] = sizeof(int);
+				break;
+			case REALSXP :
+				INTEGER(jsizes)[i] = sizeof(double);
+  			break;
+			case STRSXP :
+				INTEGER(jsizes)[i] = sizeof(SEXP);
+				break;
+			default:
+				error("only integer,double,logical and character vectors are allowed so far. Type %d would need to be added.", TYPEOF(VECTOR_ELT(SD, i)));
+		}
+  }
+  ansloc = 0;
+	for(i = 0; i < ngrp; i++) {
 		if (length(order)==0) {
 			for (j=0; j<length(SD); j++) {
 				memcpy(	(char *)DATAPTR(VECTOR_ELT(SD,j)),
@@ -65,10 +86,36 @@ SEXP dogroups(SEXP dt, SEXP SD, SEXP dtcols, SEXP order, SEXP starts, SEXP lens,
 				}
 			}			
 		}
-		for (j=0; j<length(SD); j++) {  SETLENGTH(VECTOR_ELT(SD,j),INTEGER(lens)[i]); }
-    SET_VECTOR_ELT(ans, i, eval(jexp, env));
+		for (j=0; j<length(SD); j++) SETLENGTH(VECTOR_ELT(SD,j),INTEGER(lens)[i]);
+  	PROTECT(jval = eval(jexp, env));
+		if (length(jval) != length(ans)) error("j doesn't evaluate to the same number of columns for each group");  // this would be a problem even if we unlisted afterwards. This way the user finds out earlier though so he can fix and rerun sooner.
+    maxn = 0;
+		for (j=0; j<length(jval); j++) {
+		  thislen = LENGTH(VECTOR_ELT(jval,j));
+		  maxn = thislen>maxn ? thislen : maxn;
+		  if (TYPEOF(VECTOR_ELT(jval, j)) != TYPEOF(VECTOR_ELT(ans, j))) error("columns of j don't evaluate to consistent types for each group");
+		}
+		if (ansloc + maxn > INTEGER(byretn)[0]) error("Didn't allocate enough rows. Must grow ans (to implement as we don't want default slow grow)");
+		for (j=0; j<length(jval); j++) {
+		  thisansloc = ansloc;
+		  thislen = LENGTH(VECTOR_ELT(jval,j));
+		  if (maxn%thislen != 0) error("maxn (%d) is not exact multiple of this j column's length (%d)",maxn,thislen); 
+		  for (r=0; r<(maxn/thislen); r++) {
+        memcpy(	(char *)DATAPTR(VECTOR_ELT(ans,j)) + thisansloc*INTEGER(jsizes)[j],
+              (char *)DATAPTR(VECTOR_ELT(jval,j)),
+						  thislen * INTEGER(jsizes)[j]);
+			  thisansloc += thislen;
+			}
+		}
+		ansloc += maxn; 
+		UNPROTECT(1);  // do we really need the PROTECT/UNPROTECT given the macros above won't gc() ?
 	}
-	UNPROTECT(2);
+	if (ansloc > INTEGER(byretn)[0]) error("Logical error as we should have stopped above before writing the last jval");
+	if (ansloc < INTEGER(byretn)[0]) {
+	  warning("Wrote less rows than allocated. byretn=%d but wrote %d rows",INTEGER(byretn)[0],ansloc);
+	  for (j=0; j<length(ans); j++) SETLENGTH(VECTOR_ELT(ans,j),ansloc);
+	}
+	UNPROTECT(3);
 	return(ans);
 }
 
