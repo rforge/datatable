761e696dfc63794bc30405c09a47f618289f4c18
mdowle <mdowle@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-01 23:53:42 +0000

    HTML syntax fix to homepage.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/datatable@113 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -21,23 +21,9 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
   </head>
 
-<body>
-
-<! --- R-Forge Logo --- >
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
-</table>
-
-<!-- own website starts here, the following may be changed as you like -->
-
-<h2>Welcome to data.table project!</h2>
-
 <frameset cols="*,400">
   <frame src="left.htm" />
   <frame src="right.htm" />
 </frameset>
-
-</body>
 </html>
 
